378046fd9426741d626864604ea80b65bb77ecfc
Arisa <bluelotus.melody@gmail.com>
2023-07-27 12:03:01 +0000

    tax.jsを上書き

--- tax.js
+++ tax.js
@@ -3,3 +3,10 @@ let inputValue = prompt("消費税を計算します。");
 let x = inputValue * tax;
 let afterTax = Math.floor(x);
 alert(afterTax + "円");
+
+if (1) {
+    let i = 110;
+    console.log(tax);
+    console.log(i);
+}
+console.log(i);
