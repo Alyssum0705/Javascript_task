04a3bb917deaf8d533541b2dfd64a735ed01c263
Arisa <bluelotus.melody@gmail.com>
2023-10-27 18:39:44 +0000

    課題　11　DOM　修正

--- 11_18.js
+++ 11_18.js
@@ -2,9 +2,6 @@ let e1 = document.getElementById('p1');
 let e2 = document.getElementById('p2');
 let e3 = document.getElementById('p3');
 
-e1.tagName = 'input';
-e1.type = 'text';
-e1.innerText = 'テスト';
-console.log(e1.type);
+e1.outerHTML = '<input type="text" id="p1" name="テスト"></input>';
 e2.innerText = 'テキスト文字列';
 e3.id = 'p5';
